3b57369d34e0e496f1dc9a008e8f667076553eae
Devender Dhyani <dev.dhyani2024@gmail.com>
2024-04-11 12:43:17 +0530

    Random-Color-Generator & Java-Script-2 deleted for this branch

--- java-script/countdown.js
+++ java-script/countdown.js
@@ -1,1 +1,19 @@
-console.log("working")
+const getcolor = () => {
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomHex  = "#" + randomNumber.toString(16);
+    document.getElementById("box-1").style.backgroundColor = randomHex;
+
+    const randomNumber1 = Math.floor(Math.random() * 16777215);
+    const randomHex1  = "#" + randomNumber1.toString(16);
+    document.getElementById("box-2").style.backgroundColor = randomHex1;
+}
+
+
+document.getElementById("btn").addEventListener(
+
+    "click",
+    getcolor
+
+)
+
+getcolor();
