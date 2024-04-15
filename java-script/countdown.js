54fd84b0736a3713a2ea0339b697526f022d2610
Devender Dhyani <dev.dhyani2024@gmail.com>
2024-04-15 15:54:14 +0530

    Calculator-Created-Successfully

---
+++ java-script/countdown.js
@@ -0,0 +1,59 @@
+// let x = prompt("enter your numner");
+
+
+// let y = "devender Dhyani";
+//  y = y.toUpperCase();
+// console.log(y);
+// console.log(typeof(y));
+
+
+
+// let x = prompt("Enter Your Full Name")
+// let x1 = x.replace(/\s/g, '')
+// console.log(`@${x1}${x1.length}`)
+
+
+
+
+// let x = "10"
+// let y = 10
+// z = y+x
+// console.log(z)
+// console.log(typeof(z))
+
+
+
+// const variable = [10,20,30,40,50,60]
+// let y = 0;
+// console.log(typeof(y))
+// for(let score of variable){
+//     y = y + score
+//     console.log(typeof(score))
+//     console.log(typeof(y))
+// }
+// console.log(y/variable.length)
+
+
+// const variable = [1,2,3,4,5,6,7,8,9,10]
+// const variable2 = variable.slice(1,-2)
+// console.log("V2 = ",variable2)
+
+
+// const variable = [1,2,3,4,5,6,7,8,9,10]
+// const variable2 = variable.splice(4,3,101,102,103)
+// console.log("V1 = ",variable)
+
+
+// let x = document.getElementById("display1")
+
+// const displaying = () => {
+//    console.log("Working")
+// }
+
+//  x.addEventListener(
+//     'click',
+//     displaying()
+//  )
+
+
+
