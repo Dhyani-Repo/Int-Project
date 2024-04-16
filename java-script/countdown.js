1a40c7d4759683c0f7a415331fe699213f2b6635
Devender Dhyani <dev.dhyani2024@gmail.com>
2024-04-16 21:36:38 +0530

    countdows.js compare issue on vscode

--- java-script/countdown.js
+++ java-script/countdown.js
@@ -1,19 +1,59 @@
-const getcolor = () => {
-    const randomNumber = Math.floor(Math.random() * 16777215);
-    const randomHex  = "#" + randomNumber.toString(16);
-    document.getElementById("box-1").style.backgroundColor = randomHex;
+const QuestionBank = [
+    {
+        que:"Which one of them is a markup language ?",
+        a: "HTML",
+        b: "Java",
+        c: "PHP",
+        d : "Python",
+        correct : "a",
+    },
+    {
+        'que':"Which one of them is a programming language ?",
+        'a': "HTML",
+        'b': "css",
+        'c': "tailwind",
+        'd' : "Python",
+        'correct' : "d",
+    },
+    {
+        'que':"Which one of them is a robust language ?",
+        'a': "c++",
+        'b': "Java",
+        'c': "PHP",
+        'd' : "Python",
+        'correct' : "b",
+    }
+]
 
-    const randomNumber1 = Math.floor(Math.random() * 16777215);
-    const randomHex1  = "#" + randomNumber1.toString(16);
-    document.getElementById("box-2").style.backgroundColor = randomHex1;
+let index = 0;
+const  QueBox= document.getElementById("para");
+const inputs = document.querySelectorAll("input");
+const LoadQuestion = () => {
+    const data = QuestionBank[index];
+    console.log(data);
+    QueBox.innerText = data.que;
+
+    inputs[0].nextElementSibling.innerText = data.a;             
+    inputs[1].nextElementSibling.innerText = data.b;             
+    inputs[2].nextElementSibling.innerText = data.c;             
+    inputs[3].nextElementSibling.innerText = data.d;             
+  
+}
+document.getElementById("btn").addEventListener('click',nextQuestionPlease)
+
+function nextQuestionPlease(){
+    index++;
+    if(index < QuestionBank.length ){
+        LoadQuestion();
+    }else{
+        document.getElementById("btn").innerText = "Done";
+    }
 }
 
+LoadQuestion();     
 
-document.getElementById("btn").addEventListener(
 
-    "click",
-    getcolor
 
-)
 
-getcolor();
+
+
